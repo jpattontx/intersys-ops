54b7be0a316d58284232d875a5bd4d7c498139e7
Jason Patton <jason@nemacode.com>
2016-10-07 10:44:42 -0500

    Added working elasticsearch::master and elasticsearch:client recipes.

--- chef-repo/cookbooks/elasticsearch/recipes/client.rb
+++ chef-repo/cookbooks/elasticsearch/recipes/client.rb
@@ -13,6 +13,11 @@ include_recipe "elasticsearch"
   end
 end
 
+# Copy shield config directory
+bash 'copy_shield_config' do
+  code "cp -r #{node.elasticsearch[:path][:home]}/config/shield #{node.elasticsearch.client[:path][:conf]}/"
+end
+
 # Update elasticsearch.yml
 template "#{node.elasticsearch.client[:path][:conf]}/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
@@ -22,6 +27,50 @@ template "#{node.elasticsearch.client[:path][:conf]}/elasticsearch.yml" do
   variables(
       :nodetype => "client",
       :is_master => "false",
-      :is_data => "false"
+      :is_data => "true"
   )
+end
+
+
+
+# Env setup script
+template "#{node.elasticsearch.client[:path][:conf]}/elasticsearch-env.sh" do
+  source "environment.sh.erb"
+  owner "#{node.elasticsearch[:user]}"
+  group "#{node.elasticsearch[:group]}"
+  mode "0755"
+  variables(
+      :nodetype => "client"
+  )
+end
+
+
+# Logging config
+template "#{node.elasticsearch.client[:path][:conf]}/logging.yml" do
+  source "logging.yml.erb"
+  owner "#{node.elasticsearch[:user]}"
+  group "#{node.elasticsearch[:group]}"
+  mode "0755"
+  variables(
+      :nodetype => "client"
+  )
+end
+
+
+# Init script for systemd
+template "/etc/systemd/system/#{node.elasticsearch.client[:service][:name]}.service" do
+  source 'sysd-init.erb'
+  variables(
+      nodetype: "client"
+  )
+  cookbook 'elasticsearch'
+  owner 'root'
+  group 'root'
+  mode '0644'
+#  notifies :run, 'execute[Load systemd unit file]', :immediately
+end
+
+service "#{node.elasticsearch.client[:service][:name]}.service" do
+  supports :status => true, :start => true, :stop => true, :restart => true
+  action [ :enable ]
 end
--- chef-repo/cookbooks/elasticsearch/recipes/master.rb
+++ chef-repo/cookbooks/elasticsearch/recipes/master.rb
@@ -13,6 +13,11 @@ include_recipe "elasticsearch"
   end
 end
 
+# Copy shield config directory
+bash 'copy_shield_config' do
+  code "cp -r #{node.elasticsearch[:path][:home]}/config/shield #{node.elasticsearch.master[:path][:conf]}/"
+end
+
 # Update elasticsearch.yml
 template "#{node.elasticsearch.master[:path][:conf]}/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
@@ -24,4 +29,48 @@ template "#{node.elasticsearch.master[:path][:conf]}/elasticsearch.yml" do
       :is_master => "true",
       :is_data => "false"
   )
+end
+
+
+
+# Env setup script
+template "#{node.elasticsearch.master[:path][:conf]}/elasticsearch-env.sh" do
+  source "environment.sh.erb"
+  owner "#{node.elasticsearch[:user]}"
+  group "#{node.elasticsearch[:group]}"
+  mode "0755"
+  variables(
+      :nodetype => "master"
+  )
+end
+
+
+# Logging config
+template "#{node.elasticsearch.master[:path][:conf]}/logging.yml" do
+  source "logging.yml.erb"
+  owner "#{node.elasticsearch[:user]}"
+  group "#{node.elasticsearch[:group]}"
+  mode "0755"
+  variables(
+      :nodetype => "master"
+  )
+end
+
+
+# Init script for systemd
+template "/etc/systemd/system/#{node.elasticsearch.master[:service][:name]}.service" do
+  source 'sysd-init.erb'
+  variables(
+      nodetype: "master"
+  )
+  cookbook 'elasticsearch'
+  owner 'root'
+  group 'root'
+  mode '0644'
+#  notifies :run, 'execute[Load systemd unit file]', :immediately
+end
+
+service "#{node.elasticsearch.master[:service][:name]}.service" do
+  supports :status => true, :start => true, :stop => true, :restart => true
+  action [ :enable ]
 end
